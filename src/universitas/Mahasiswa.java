25dfd758966c4721251079cf0e33be31ac363812
hendanugrahpasae <hendanugrahpasae@gmail.com>
2023-06-13 14:33:29 +0800

    Melengkapi class Mahasiswa

--- src/universitas/Mahasiswa.java
+++ src/universitas/Mahasiswa.java
@@ -11,8 +11,37 @@ public class Mahasiswa {
         this.prodi = prodi;
     }
     
+    // Setter untuk npm
+    public void setNpm(String npm) {
+        this.npm = npm;
+    }
+    
+    // Getter untuk npm
+    public String getNpm() {
+        return npm;
+    }
+    
+    // Setter untuk nama
+    public void setNama(String nama) {
+        this.nama = nama;
+    }
+    
+    // Getter untuk nama
+    public String getNama() {
+        return nama;
+    }
+    
+    // Setter untuk prodi
+    public void setProdi(String prodi) {
+        this.prodi = prodi;
+    }
+    
+    // Getter untuk prodi
+    public String getProdi() {
+        return prodi;
+    }
+    
     public String getDetail() {
         return "NPM: " + npm + "\nNama: " + nama + "\nProdi: " + prodi;
     }
 }
-
